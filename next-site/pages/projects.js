ed5919fe3ed942dafc454300480f15ba82fc285d
watching-dev <coderefactoring90@gmail.com>
2023-12-20 15:00:36 +0900

    [feat] getServerSideProps() 로 변경_노션 데이터 변경시 반영 되도록

--- next-site/pages/projects.js
+++ next-site/pages/projects.js
@@ -28,7 +28,49 @@ export default function Projects({ projects }) {
   );
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       accept: "application/json",
+//       "Notion-Version": "2022-06-28",
+//       "content-type": "application/json",
+//       Authorization: `Bearer ${TOKEN}`,
+//     },
+//     body: JSON.stringify({
+//       sorts: [
+//         {
+//           property: "이름",
+//           direction: "ascending",
+//         },
+//       ],
+//       page_size: 100, // 100페이지까지 가져와라
+//     }),
+//   };
+
+//   const res = await fetch(
+//     `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+//     options
+//   );
+//   const projects = await res.json();
+//   console.log(projects);
+//   console.log(JSON.stringify(projects.results)); // 이걸로 [object Object] 안 데이터를 볼 수 있음
+
+//   const projectNames = projects.results.map(
+//     (project) => project.properties.이름.title[0].plain_text
+//     // .title[0].plain_text
+//   );
+
+//   console.log(JSON.stringify(projectNames)); // 이걸로 [object Object] 안 데이터를 볼 수 있음
+
+//   console.log(`projectNames: ${projectNames}`);
+
+//   return {
+//     props: { projects },
+//   };
+// }
+
+export async function getServerSideProps() {
   const options = {
     method: "POST",
     headers: {
